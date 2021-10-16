58cd6ff2689912c3b717fbf024c135f35213442e
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2021-10-16 18:19:47 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,52 +1,56 @@
-$(document).ready(function () {
-     //Ẩn hiện title HEADER
-     $('.header-right-wrap').click(function (e) { 
+$(document).ready(function() {
+    //Ẩn hiện title HEADER
+    $('.header-right-wrap').click(function(e) {
         e.preventDefault();
         $(this).find('.header-right-title').toggle('show');
         $(this).siblings().toggle('hide');
     });
 
     //ĐÓNG TITLE WELCOME TO FRANCO
-    $('.content-welcome').click(function (e) { 
+    $('.content-welcome').click(function(e) {
         e.preventDefault();
         $(this).find('.content-welcome-title').toggle('show');
     });
 
-    $('.button-search').click(function (e) { 
+    $('.button-search').click(function(e) {
         e.preventDefault();
-       $('.form').show();
+        $('.form').show();
     });
 
 
-//ĐÓNG FORM TÌM KIẾM
-    $('.close').click(function (e) { 
+    //ĐÓNG FORM TÌM KIẾM
+    $('.close').click(function(e) {
         e.preventDefault();
-       $('.form').hide();
+        $('.form').hide();
     });
 
-//SHOW ICON HEART AND CART
-    $('.category').click(function (e) { 
+    //SHOW ICON HEART AND CART
+    $('.category').click(function(e) {
         e.preventDefault();
-        $('.button-love-and-cart').show();
-    });
-     
-//CLOSE ICON HEART AND CART
-$('.close-heart-and-cart').click(function (e) { 
-    e.preventDefault();
-    $('.button-love-and-cart').hide();
-}); 
-// MỞ NAVIGATION  MOBILE
-    $('.nav-title').click(function (e) { 
-        e.preventDefault();
-       $('.navigation').addClass('active');
+        $('.button-love-and-cart').addClass('active');
+        $('.cover').addClass('active');
     });
 
-//ĐÓNG NAVIGATION MOBILE
-    $('.button-close-navigation').click(function (e) { 
+    //CLOSE ICON HEART AND CART
+    $('.cover').click(function(e) {
         e.preventDefault();
-       $('.navigation').removeClass('active');
+        $('.button-love-and-cart').removeClass('active');
+        $('.cover').removeClass('active');
+    });
+    // MỞ NAVIGATION  MOBILE
+    $('.nav-title').click(function(e) {
+        e.preventDefault();
+        $('.navigation').addClass('active');
+        $('.cover').addClass('active');
+    });
+
+    //ĐÓNG NAVIGATION MOBILE
+    $('.cover').click(function(e) {
+        e.preventDefault();
+        $('.navigation').removeClass('active');
+        $('.cover').removeClass('active');
     });
 
 
-   
+
 });
